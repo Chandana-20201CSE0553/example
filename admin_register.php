dc57020c9a8d2164c752fb81197795e1c9b25d48
Chandana-20201CSE0553 <149507615+Chandana-20201CSE0553@users.noreply.github.com>
2024-01-13 21:08:58 +0530

    Add files via upload

---
+++ admin_register.php
@@ -0,0 +1,97 @@
+<html lang="en">
+   <?php include("php_include/head.php")?>
+   <body >
+      <div id="loader-wrapper">
+         <div id="loader">
+            <div class="loader-inner"></div>
+         </div>
+      </div>
+      <div id="page" class="page">
+         <header id="header" class="header" >
+                     
+                     
+                     <div class="wsmainfull menu clearfix" >
+                        <div class="wsmainwp clearfix" >
+                          <div class="desktoplogo"><a href="#hero-2"><img src="images/text-1702913677293.png"  width="300" height="40" alt="header-logo"></a></div>
+                           <nav class="wsmenu clearfix">
+                              <ul class="wsmenu-list">
+                                
+                                
+                                 <li aria-haspopup="true">
+                                    <a href="admin.php"><font size="4">Back</font></a>
+                                 </li>
+                                 
+                                
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </header>
+                  <section id="hero-2" class="hero-section division">
+            <div class="slider blue-nav">
+               <ul class="slides">
+                  <li id="slide-1">
+                     <img src="images/mlp.jpg" alt="slide-background">
+                     <div class="caption d-flex align-items-center left-align">
+                        <div class="container">
+                           <div class="row">
+         <center>  <h3>CHOOSE A CATEGORY TO REGISTER</h3>
+              <br>
+
+
+                                 
+                                    <button class="btn btn-light" style="border-color:black;"><font size="4"><a href="hospital_register.php">Hospital</a></font></button>
+                              <br><br>
+                                   <button class="btn btn-light" style="border-color:black;"><font size="4"><a href="nurse_register.php">Nurse</a></font></button>
+                               <br><br>
+                               <button class="btn btn-light" style="border-color:black;"><font size="4"><a href="doc_register.php">Doctor</a></font></button>
+                            <br><br>
+                          
+
+                             </center>            
+
+
+
+
+
+
+                           </div>
+                        </div>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </section>
+       
+
+      </div>
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>	
+      <script src="js/modernizr.custom.js"></script>
+      <script src="js/jquery.easing.js"></script>
+      <script src="js/jquery.appear.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>	
+      <script src="js/menu.js"></script>
+      <script src="js/sticky.js"></script>
+      <script src="js/jquery.scrollto.js"></script>
+      <script src="js/materialize.js"></script>	
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.magnific-popup.min.js"></script>	
+      <script src="js/imagesloaded.pkgd.min.js"></script>
+      <script src="js/isotope.pkgd.min.js"></script>
+      <script src="js/hero-form.js"></script>
+      <script src="js/contact-form.js"></script>
+      <script src="js/comment-form.js"></script>
+      <script src="js/appointment-form.js"></script>
+      <script src="js/jquery.datetimepicker.full.js"></script>		
+      <script src="js/jquery.validate.min.js"></script>	
+      <script src="js/jquery.ajaxchimp.min.js"></script>
+      <script src="js/wow.js"></script>	
+      <script src="js/custom.js"></script>
+      <script> 
+         new WOW().init();
+      </script>
+      <script src="js/changer.js"></script>
+      <script defer src="js/styleswitch.js"></script>	
+   </body>
+</html>
